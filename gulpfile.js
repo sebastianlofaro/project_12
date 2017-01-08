56db93e1d5125b86071974e039651fe764d636dc
sebastianlofaro <sebastianlofaro@me.com>
2017-01-08 13:15:01 -0600

    Skills
    Added list of skills to projects
    
    added watch and livereload to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@
 
 var gulp     = require('gulp'),
     sass     = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css')
-    minify   = require('gulp-minify');
+    cleanCSS = require('gulp-clean-css'),
+    minify   = require('gulp-minify'),
+    livereload = require('gulp-livereload');
 
 
 gulp.task('sass', function() {
   return gulp.src('./styles/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./styles/css'));
+    .pipe(gulp.dest('./styles/css'))
+    .pipe(livereload());
 });
 
 gulp.task('minify-css', function() {
@@ -18,7 +20,10 @@ gulp.task('minify-css', function() {
   .pipe(gulp.dest('styles/css'));
 });
 
-
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(['./styles/sass/**/*.scss', 'index.html'], ['sass']);
+});
 
 gulp.task('default', function() {
   gulp.watch('./styles/sass/**/*.scss', ['sass']);
